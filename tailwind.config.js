80db9e5d69b4987d2d1705dc9733f7c65e512607
UIT-21522029 <96015169+Napeno@users.noreply.github.com>
2025-04-21 16:07:17 +0700

    First init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#1D7595",
+        main_hover: "#276880",
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
+        secondary: "#0f3f50",
+        text: "#323a3f",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
